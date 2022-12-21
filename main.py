9ac8b2cb95a3c19365c0b7c7c208831d12203a28
nfonteyne <nathan.fonteyne59@gmail.com>
2022-12-21 18:53:51 +0100

    modification css html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -20,6 +20,11 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250924533-1"></script>
 
 @app.route('/logger', methods=["GET"])
 def log():
-    return app.logger.info()
+    todisplay = render_template("main.html")
+    todisplay = render_template("film_all.html")
+    todisplay+=f"<div class='card'>\n<h1> logger</h1>\n</div>\n"
+    script = """
+    <script> console.log("logger") </script>"""
+    return "I'm a logger" + script
 
     
